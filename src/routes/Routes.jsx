b5c33d4666c40164b64a29c44bc4827d4c950e61
LitanMolla <litanmolla9@gmail.com>
2025-10-07 03:20:12 +0600

    blank route pages added

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -2,6 +2,11 @@ import React from 'react'
 import { createBrowserRouter } from 'react-router';
 import Root from '../layout/Root/Root';
 import NotFound from '../pages/NotFound/NotFound';
+import About from '../pages/About/About';
+import Contact from '../pages/Contact/Contact';
+import Blog from '../pages/Blog/Blog';
+import Shop from '../pages/Shop/Shop';
+import Home from '../pages/Home/Home';
 
 const Routes = createBrowserRouter([
     {
@@ -9,7 +14,26 @@ const Routes = createBrowserRouter([
         Component: Root,
         errorElement: <NotFound/>,
         children:[
-            
+            {
+                index: true,
+                Component: Home
+            },
+            {
+                path: 'about',
+                Component: About
+            },
+            {
+                path: 'contact',
+                Component: Contact
+            },
+            {
+                path: 'blog',
+                Component: Blog
+            },
+            {
+                path: 'shop',
+                Component: Shop
+            }
         ]
     }
 ])
